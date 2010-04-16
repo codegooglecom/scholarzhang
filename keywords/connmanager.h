1ba798f2c20fef7bda8ea0e6888066940dc3e475
yingyingcui.scholarzhang <yingyingcui.scholarzhang@dcf2ae5c-931b-11de-97c9-d5744c8abab9>
2010-04-16 03:46:33 +0000

    guk_server

--- keywords/connmanager.h
+++ keywords/connmanager.h
@@ -4,6 +4,7 @@
 #include "dstmaintain.h"
 
 #define DEFAULT_CONN 10000
+#define RESULT_ERROR 0x04
 typedef void (*gk_callback_f)(char *content, char result, void *arg);
 
 int gk_add_context(char * const content, const int length, char *const result, const int type, gk_callback_f cb, void *arg);
